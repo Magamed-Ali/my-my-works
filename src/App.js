7e39e4cb9e594841de6ab5fa3336d55a38a369ce
gazmagamaev <magamedali93@mail.ru>
2022-02-11 21:27:52 +0300

    my progect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useSelector} from "react-redux";
+import './style.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const project = useSelector(state => state.project)
+
+    return (
+        <div className="container">
+            <div className="title"><p>Мои работы</p>
+                <hr/>
+            </div>
+            <div className="row gy-4">
+                {project.map(item => {
+                    return (
+                        <div className="col-sm-6 col-lg-4">
+                            <div className="wrapper-card">
+                                <h5>{item.title}</h5>
+                                <p>{item.description}</p>
+                                <div className="wrapper-card__boxlink">
+                                    <span className="heroku"><a href={item.linkDemo}
+                                                              target="_blank">Перейти</a></span>
+                                    <span className="git"><a href={item.linkGit}
+                                                          target="_blank">Посмотреть код</a></span>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/store.js
@@ -0,0 +1,52 @@
+
+const initState = {
+    project: [
+        {
+            title: 'Counter 1',
+            description: "Название проекта",
+            linkDemo: "/",
+            linkGit: "/"
+        },
+        {
+            title: 'Counter 2',
+            description: "Название проекта",
+            linkDemo: "",
+            linkGit: ""
+        },
+        {
+            title: 'Counter 3',
+            description: "Название проекта",
+            linkDemo: "",
+            linkGit: ""
+        },
+        {
+            title: 'Counter 4',
+            description: "Название проекта",
+            linkDemo: "",
+            linkGit: ""
+        },
+        {
+            title: 'Counter 5',
+            description: "Название проекта",
+            linkDemo: "",
+            linkGit: ""
+        },
+        {
+            title: 'Counter 6',
+            description: "Название проекта",
+            linkDemo: "",
+            linkGit: ""
+        }
+        ]
+}
+
+export const reduser = (state = initState, action) => {
+
+    switch (action.type){
+
+        default:
+            return state
+
+
+    }
+}
