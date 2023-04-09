09d53193369e5010b779f7ab3d30d2529be24ecb
Magamed-Ali <80570590+Magamed-Ali@users.noreply.github.com>
2023-04-09 23:52:36 +0300

    add project Watch

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import img1 from "./work1.png";
 import img2 from "./work2.jpg";
+import img3 from "./work2.jpg";
 
 const initState = {
     project: [
@@ -8,21 +9,21 @@ const initState = {
             description: "Небольшое приложение на React, которое умеет регистрировать на вакцинацию от КОВИД-19.",
             linkDemo: "https://registration-for-vaccination-3i4i.vercel.app/",
             linkGit: "https://github.com/Magamed-Ali/registration_for_vaccination",
-            img: img1
+            img: img1;
         },
         {
             title: 'Movies project',
             description: "Поиск фильм (по названию фильма, по жанре, серийные фильмы)",
             linkDemo: "https://magamed-ali.github.io/movies-project/",
             linkGit: "https://github.com/Magamed-Ali/movies-project",
-            img: img2
+            img: img2;
         },
         {
-            title: 'Counter 3',
-            description: "Название проекта",
-            linkDemo: "",
-            linkGit: "",
-            img: ""
+            title: 'Watch',
+            description: "Текущее время (+3)",
+            linkDemo: "https://magamed-ali.github.io/watch/",
+            linkGit: "https://github.com/Magamed-Ali/watch",
+            img: img3;
         },
         {
             title: 'Counter 4',
